df7efa86acb9773177100f81798bf0faea1e0a89
QiangZiBro <qiangzibro@gmail.com>
2021-10-15 16:26:23 +0800

    bugfix: preprocess fillna

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 from threading import Thread
 from common.io import *
+import numpy as np
 
 
 def create_new_table_for_place(p):
@@ -9,23 +10,26 @@ def create_new_table_for_place(p):
 
         # 负值处理
         num = df._get_numeric_data()
-        num[num<0] = np.nan
+        num[num < 0] = np.nan
 
         if args.fillna == "mean":
             # 空值用中位数填充
-            df.fillna(df.median())
+            df = df.fillna(df.median())
         elif args.fillna == "mean":
             # 空值用均值填充
-            df.fillna(df.mean())
+            df = df.fillna(df.mean())
 
-        data[p][i].to_excel(writer, sheet_name=df.name)
+        data[p][i].to_excel(writer, sheet_name=str(i))
     writer.save()
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--fillna", type=str, default="mean", choices=["mean", "median"] )
+    parser.add_argument("--fillna",
+                        type=str,
+                        default="mean",
+                        choices=["mean", "median"])
     args = parser.parse_args()
     print("Use {} to fillna".format(args.fillna))
 
@@ -33,7 +37,7 @@ if __name__ == '__main__':
     print("data load success")
     threads = []
     for p in PLACES:
-        threads.append(Thread(target=create_new_table_for_place, args=(p,)))
+        threads.append(Thread(target=create_new_table_for_place, args=(p, )))
     for t in threads:
         t.start()
     for t in threads:
